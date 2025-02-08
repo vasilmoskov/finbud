8611eff7e66882138019f02def9b50bb3afa40ba
Vasil Moskov <moskov.vasil@gmail.com>
2025-02-08 13:21:07 +0200

    vizualize expenses in bgn

--- src/main/frontend/views/@index.tsx
+++ src/main/frontend/views/@index.tsx
@@ -1,20 +1,15 @@
 import { ViewConfig } from '@vaadin/hilla-file-router/types.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import { Cell, Legend, Pie, PieChart, Tooltip } from 'recharts';
+import { useExpenseViewState } from 'Frontend/hooks/useExpenseViewState';
+import { useEffect, useState } from 'react';
+import { Transaction } from 'Frontend/types/Transaction';
 
 export const config: ViewConfig = {
   menu: { order: 0, icon: 'line-awesome/svg/globe-solid.svg' },
   title: 'Dashboards',
   loginRequired: true,
 };
-
-const expensesData = [
-  { name: "Rent", value: 1200 },
-  { name: "Groceries", value: 600 },
-  { name: "Utilities", value: 200 },
-  { name: "Transportation", value: 150 },
-  { name: "Entertainment", value: 100 },
-];
 
 const incomesData = [
   { name: "Salary", value: 3000 },
@@ -50,10 +45,50 @@ const renderCustomizedLabel = ({ cx, cy, midAngle, outerRadius, percent, index }
 };
 
 const formatTooltipValue = (value: number) => {
-  return `${value.toFixed(2)} $`;
+  return `${value.toFixed(2)} лв.`;
+};
+
+const conversionRates: {[key: string]: number} = {
+  'лв.': 1,
+  '€': 1.96,
+  '$': 1.89,
+  '£': 2.35,
+};
+
+const convertToBGN = (amount: number, currency: string) => {
+  const rate = conversionRates[currency];
+  return amount * rate;
+};
+
+const accumulateExpensesByCategory = (expenses: Transaction[]) => {
+  const categoryMap = expenses.reduce((acc: { [key: string]: number }, expense: Transaction) => {
+    
+    const amountInBGN = convertToBGN(expense.amount, expense.currency);
+    
+    if (acc[expense.category]) {
+      acc[expense.category] += amountInBGN;
+    } else {
+      acc[expense.category] = amountInBGN;
+    }
+
+    return acc;
+  }, {});
+
+  return Object.keys(categoryMap).map(category => ({
+    name: category,
+    value: categoryMap[category]
+  }));
 };
 
 export default function DashboardsView() {
+  const {expenses} = useExpenseViewState(); 
+  const [expensesData, setExpensesData] = useState<{ name: string; value: number }[]>([]);
+
+  useEffect(() => {
+    const accumulatedExpenses = accumulateExpensesByCategory(expenses);
+    setExpensesData(accumulatedExpenses);
+  }, [expenses]);
+
   return (
     <div className="flex flex-col h-full items-center justify-center p-l text-center box-border">
       <div className="flex flex-row justify-around w-full">
