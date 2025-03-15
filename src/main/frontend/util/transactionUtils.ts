558fc2c55ca611ae3e59fc0d4d2e5b96c8d496d0
vasilmoskov <moskov.vasil@gmail.com>
2025-03-15 10:56:53 +0200

    remove unused functions

--- src/main/frontend/util/transactionUtils.ts
+++ src/main/frontend/util/transactionUtils.ts
@@ -1,19 +1,6 @@
 import { DatePickerDate } from "@vaadin/react-components";
 import { format, parse } from "date-fns";
-import { incomeCategoriesMap, currencyCodesToSigns } from "Frontend/constants/constants";
 import TransactionDto from "Frontend/generated/com/example/application/dto/TransactionDto";
-
-export const formtatCurrency = (currency: string): string => {
-    return currencyCodesToSigns[currency] || currency;
-}
-
-export const formatCategory = (category: string): string => {
-    return incomeCategoriesMap[category] || category;
-};
-
-export const formatDate = (dateString: string): string => {
-    return format(new Date(dateString), "dd MMM yyyy HH:mm:ss");
-};
 
 export const formatDateForDatePicker = (dateParts: DatePickerDate) => {
     const { year, month, day } = dateParts;
