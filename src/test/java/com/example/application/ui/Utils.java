05600eec7a4b767ccd3bed702e81ab6ec6579a75
vasilmoskov <moskov.vasil@gmail.com>
2025-03-16 12:47:09 +0200

    create user data dir for linux

--- src/test/java/com/example/application/ui/Utils.java
+++ src/test/java/com/example/application/ui/Utils.java
@@ -2,12 +2,19 @@ package com.example.application.ui;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 
 public class Utils {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Utils.class);
+
     private static WebDriver driver;
 
     private static WebDriverWait wait;
@@ -25,9 +32,26 @@ public class Utils {
     public static void initDriver() {
         String driverPath = System.getenv("WEBDRIVER_PATH");
 
+        LOGGER.info("Chrome driver path: {}", driverPath);
+
         System.setProperty("webdriver.chrome.driver", driverPath);
 
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        String osName = System.getProperty("os.name").toLowerCase();
+
+        LOGGER.info("Operating System: {}", osName);
+
+        if (osName.contains("linux")) {
+            try {
+                Path tempDir = Files.createTempDirectory("chrome-user-data");
+                options.addArguments("--user-data-dir=" + tempDir.toString());
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to create temporary user data directory", e);
+            }
+        }
+
+        driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
         driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
